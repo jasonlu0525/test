3f1e2f2572fd6fdafc7daa9f36db1a805236774f
jaosnlu <s099113302@gmail.com>
2021-08-31 15:16:12 +0800

    Update 2021-08-31T07:16:11.231Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,16 +3,16 @@
 $(function () {
   console.log('Hello Bootstrap5');
   alert(1230);
-});
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 'auto',
-  spaceBetween: 30,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true
-  },
-  grabCursor: true
-});
+}); // var swiper = new Swiper(".mySwiper", {
+//   slidesPerView: 'auto',
+//   spaceBetween: 30,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   grabCursor : true,
+// });
+
 var swiper = new Swiper(".SwiperGroup", {
   slidesPerView: 'auto',
   grid: {
